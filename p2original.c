40210bd0fa518de1065e040f8e324fa1b50576ae
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-03-11 03:38:47 +0000

    p2,p6

--- p2original.c
+++ p2original.c
@@ -1,17 +1,62 @@
 #include<stdio.h>
-typedef struct _fraction{
-  int num;
-  int den;
-}fraction;
-fraction input_fraction(){
-fraction f;
-  printf("Enter the numerator and denomenator:");
-  scanf("%d %d",&f.num,&f.den);
-  return f;
-}
-fraction largest_fraction(fraction f1,fraction f2,fraction f3)
+
+typedef struct fraction
 {
-  
+    int num,den;
+}frac;
+frac input_fraction(){
+    frac a;
+    printf("Enter the Numeraor and Denominator of fraction\n");
+    scanf("%d %d",&a.num,&a.den);
+    return a;
+}g
+int smallest_fraction(frac f1,frac f2, frac f3){
+   float a,b,c,d,e,f,g,h,i,j;
+    a=(f1.num);
+    b=(f2.num);
+    c=(f3.num);
+    e=(f1.den);
+    f=(f2.den);
+    g=(f3.den);
+    h=a/e;
+    i=b/f;
+    j=c/g;
+     if(h<i && h<j){
+        d=1;
+    }
+    else if(i<j&& i<h){
+        d=2;
+    }
+    else if(j<h && j<i){
+        d=3;
+    }
+
+    printf("%f %f\n",h,i);
+   return d;
+}
+void output(frac f1, frac f2, frac f3, int smallest){
+    if(smallest==1){
+        printf("The smallest fraction is %d/%d",f1.num,f1.den);
+    }
+    else if(smallest==2){
+         printf("The smallest fraction is %d/%d",f2.num,f2.den);
+    }
+    else if (smallest==3){
+         printf("The smallest fraction is %d/%d",f3.num,f3.den);
+    }
+    else if(smallest==0){
+         printf("all the fractions are same\n");
+    }
+}
+int main(){
+    frac f1,f2,f3;
+    int smallest;
+    f1=input_fraction();
+    f2=input_fraction();
+    f3=input_fraction();
+    smallest=smallest_fraction(f1,f2,f3);
+    output(f1,f2,f3,smallest);
+    return 0;
 }
 
-  
+
---
+++ p6original.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<string.h>
+void input_string(char a[]){
+    printf("Enter the sentence\n");
+    fgets(a,100,stdin);
+}
+int count_words(char a[]){
+    int i=0;
+    char *temp=strtok(a," ");
+    while(temp!=NULL){
+        i=i+1;
+        temp=strtok(NULL," ");
+    }
+    printf("%d\n",i);
+    return i;
+}
+void output(char a[],int no_of_words){
+    printf("The sentence has %d words",no_of_words);
+}
+int main(){
+    int n;
+    char a[100];
+    input_string(a);
+    n=count_words(a);
+    output(a,n);
+    return 0;
+}
