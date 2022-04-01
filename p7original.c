fd502a2d538a57ffd34490d074e90ef5723c445a
vishnu kumar <97020373+Vishnu9535@users.noreply.github.com>
2022-04-01 03:28:17 +0000

    p7,p8

--- p7original.c
+++ p7original.c
@@ -1,16 +1,45 @@
-#include<stdio.h>
-typedef struct _fraction
+    #include <stdio.h>
+typedef struct fraction
 {
-int num,den;
-}Fraction;
-Fraction input_fraction(){
-  Fraction f;
-  printf("enter the  fraction");
+  int num,den;
+}fraction;
+fraction input()
+{
+  fraction f;
+  printf("Enter the numerator and denomenator of the fraction:");
   scanf("%d %d",&f.num,&f.den);
   return f;
 }
-Fraction add_fractions(Fraction f1, Fraction f2)
+fraction add(fraction f1,fraction f2)
 {
-  float num1,den1,num2,den2;
-  num1=f.num;
+  fraction sum;
+  int n1,n2,d1,d2;
+  n1=f1.num;
+  n2=f2.num;
+  d1=f1.den;
+  d2=f2.den;
+  sum.den=d1*d2;
+    sum.num=(n1*d2)+(n2*d1);
+    for(int i=1;i<=sum.den;i++)
+      {
+        if(sum.den%i == 0 && sum.num%i == 0)
+        {
+            sum.den=sum.den/i;
+            sum.num=sum.num/i;
+        }
+      }
+  return sum;
+}
+void output(fraction f1,fraction f2,fraction sum)
+{
+  printf("%d/%d + %d/%d is %d/%d\n",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
+}
+int main()
+{
+  fraction f1,f2,sum;
+  f1=input();
+  f2=input();
+  sum=add(f1,f2);
+  output(f1,f2,sum);
+  return 0;
 }
